99e0888250bc8aa4f5fb7ecd057a022d7814a866
grzesiomaster <grzesiomaster@o2.pl>
2018-02-19 00:01:10 +0100

    Add project files.

---
+++ whHireCar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace whHireCar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
